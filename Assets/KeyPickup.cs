64e0c123af331077fb926bbc17bf47edb7f0b3e0
Lief Swanson <liefs@sfu.ca>
2016-07-09 21:59:50 -0700

    add key inventory + 3d sounds

---
+++ Assets/KeyPickup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class KeyPickup : MonoBehaviour {
+
+    private bool hasKey = false;
+    public Image keyImg;
+    public GameObject key;
+
+    void Start()
+    {
+        keyImg.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Key")
+        {
+            hasKey = true;
+            keyImg.enabled = true;
+            key.SetActive(false);
+        }
+    }
+}
