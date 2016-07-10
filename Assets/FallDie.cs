b036a468417335c3ad6290da1a774f69ddf2c0f4
Lief Swanson <liefs@sfu.ca>
2016-07-09 22:18:39 -0700

    add death

---
+++ Assets/FallDie.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class FallDie : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            var scene = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
