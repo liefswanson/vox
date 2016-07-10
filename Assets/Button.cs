481a05f43beaeb9286285142d05237bb7fcadf25
Lief Swanson <liefs@sfu.ca>
2016-07-09 18:07:23 -0700

    add platform and button

---
+++ Assets/Button.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+    public PlatformController target;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            target.Activate();
+            Debug.Log("derp");
+        }
+    }
+}
---
+++ Assets/PlatformController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlatformController : MonoBehaviour {
+
+    public Transform target;
+    public AudioClip clipToMatch;
+    private float speed;
+    private bool activated = false;
+
+	void Start () {
+        var time = clipToMatch.length;
+        var distance = Vector3.Distance(transform.position, target.position);
+        speed = distance / time;
+	}
+	
+	void Update () {
+        if (activated)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+	}
+
+    public void Activate()
+    {
+        activated = true;
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.PlayOneShot(clipToMatch);
+    }
+}
