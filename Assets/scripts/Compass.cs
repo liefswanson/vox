7df18e2209d43787431fdcd42ba7568061f559a1
Lief Swanson <liefs@sfu.ca>
2016-07-10 23:23:12 -0700

    optional add compass

---
+++ Assets/scripts/Compass.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour {
+
+    public GameObject player;
+    private Image needle;
+
+    void Start()
+    {
+        needle = GetComponent<Image>();
+    }
+
+	void Update () {
+        needle.transform.eulerAngles = new Vector3(0, 0, -player.transform.eulerAngles.y);
+	}
+}
